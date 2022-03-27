214c4bd151e5306b1a4d0b2872b80b9edc35732d
JieJie <yahetw@gmail.com>
2022-03-27 23:53:49 +0800

    Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code her
+        System.out.println("Hi");
+
     }
+
+
 }
