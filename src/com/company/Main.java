017abef1d3c3b04f5fbcf5f3e0cb70042158eab6
JieJie <yahetw@gmail.com>
2022-03-28 00:03:54 +0800

    Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code her
         System.out.println("Hi");
-
+        System.out.println("You can do it, you can do everying");
     }
 
 
