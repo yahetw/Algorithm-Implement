71cbb4366bd48ba35f58c0842052e7ffa89ba017
JieJie <yahetw@gmail.com>
2022-06-01 22:04:05 +0800

    更新

--- src/com/company/HW2.java
+++ src/com/company/HW2.java
@@ -1,19 +1,16 @@
 package com.company;
 //TODO: M104020052 張孫杰
 //TODO: This assignment aims to write a function simulate to allocate the course process by student’s preference.
-import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class HW2 {
     
   private HW2() {}
 
-  public static class Heap{
+  public class Heap {
     private Heap() { }
 
-    public static void sort(Course[] pq) {
+     public static void sort(Student[] pq) {
       int n = pq.length;
 
       // heapify phase
@@ -21,23 +18,26 @@ public class HW2 {
         sink(pq, k, n);
 
       // sortdown phase
-      // write your code here
       int k = n;
+      boolean h2_max_r= less(pq,2,3); // we save the maximum of height 2 in advance, so we don't need to compare it later.
+      while (k > 1) {
 
-      while(n > 1) {
-        exch(pq, 1, n--);  // 最大的跟最後面換
-        if (less(pq, 2, 3)) {  //如果左子樹比較小
-          exch(pq, 1, 3);  //將右子樹交換到root
-          sink(pq, 3, n);  //將右子樹sink
+        exch(pq, 1, k--);
+        if(h2_max_r) {
+          exch(pq,1,3);
+
+          sink(pq,3,k);
+        }else {
+          exch(pq,1,2);
+
+          sink(pq,2,k);
         }
-        else {
-          exch(pq, 1, 2);  //相反
-          sink(pq, 2, n);
-        }
+        h2_max_r= less(pq,2,3); // after we finish it, we need to compare it again
+        //sink(pq, 1, k);
       }
     }
 
-    private static void sink(Course[] pq, int k, int n) {
+    private static void sink(Student[] pq, int k, int n) {
       while (2*k <= n) {
         int j = 2*k;
         if (j < n && less(pq, j, j+1)) j++;
@@ -47,8 +47,10 @@ public class HW2 {
       }
     }
 
-    private static boolean less(Course[] pq, int i, int j) {
-      return pq[i-1].compareTo(pq[j-1]) < 0;
+    private static boolean less(Student[] pq, int i, int j) {
+      
+
+      return pq[i-1].year- (pq[j-1].year) > 0;
     }
 
     private static void exch(Object[] pq, int i, int j) {
@@ -69,7 +71,6 @@ public class HW2 {
         this.name = name;
         this.candidate = new Student[limit_number];
     }
-    
   }
   
   public static class Student{
@@ -163,38 +164,56 @@ public class HW2 {
   // Simulate courses allocating process
   private static Course[] simulate(Student[] students, Course[] courses) {
 
-//    for(Course course:courses) {
-//      System.out.println(course.name+" : ");
-////      System.out.println(Arrays.toString(course.candidate));
-//
-//    }
+    // 2階段排序Comparator 參考自 : https://blog.csdn.net/HD243608836/article/details/102535299
 
-    // 依照年級進行switch case，大的年級優先
 
-    // 應該先看志願序，如果志願相同，比年級，再比student id
-    // 假如年級相同，會先比student id
-    for(Student student: students){
-      switch (student.year){
-        case 1:
-          break;
-        case 2:
-          break;
-        case 3:
-          break;
-        case 4:
+    // 新增所有students物件到maxHeap中，新增完後maxHeap中的物件會是已排序好的狀態
+    Heap.sort(students);
 
-          break;
+    //因課程編號不會跳號，所以從id為1的課程開始選課
+    for(int i=1;i<= courses.length;i++ ){
+      //排序好後，課程編號為1的先分發，即挑選課程志願序為1的人往前排，其他人次序往後
+      Student[] students_course_selected = new Student[students.length];  //建立一個專屬課程編號i的待選上的學生排序清單
+      Queue<Student> queue_not_my_preference = new LinkedList<>();  // 用來處理志願序不為i的情況，其他人次序往後
+      Queue<Student> queue_all_fine = new LinkedList<>(); // 用來處理志願序長度不一的情況，如{1}、{1,2}
+      int stu_length =0;  //已選上的人數
+
+      // 依照志願進行選課
+      for (Student student: students){
+        // 如果該名學生的志願序有寫滿
+        if (student.preference.length == courses.length){
+          if (student.preference[0]==i){
+            // 第一個志願的為
+            students_course_selected[stu_length] = student;
+            stu_length++;
+          }else queue_not_my_preference.offer(student);
+        }else if (student.preference.length==0){
+          queue_all_fine.offer(student);
+        }
+        // 空志願序擺前面
+        while (!queue_all_fine.isEmpty()){
+          students_course_selected[stu_length++] = queue_all_fine.poll();
+        }
+        // 非空志願序擺後面
+        while (!queue_not_my_preference.isEmpty()){
+          students_course_selected[stu_length++] = queue_not_my_preference.poll();
+        }
       }
+      // 放入選到該課程的學生
+      Student[] assign_students = new Student[courses[i-1].candidate.length];
+      for (int j=0; j<assign_students.length; j++){
+        assign_students[j] = students_course_selected[j];
+      }
+      courses[i-1].candidate =assign_students;
+
+      int N = students.length;
+      for (int j= 0;j< assign_students.length; j++){
+        Heap.exch(students,1,N--);
+        Heap.sink(students,1,N);
+      }
+
+      //如果課程人數沒滿，進入第二階段迴圈選課
     }
-
-    Student[] assign_students = new Student[3];
-    assign_students[0] = students[0];
-    assign_students[1] = students[1];
-
-    courses[0].candidate =assign_students;
-
-    System.out.println(students[0].id);
-
 
     return courses;
   }
