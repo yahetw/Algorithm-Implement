e1799c3bc0f4b216bf3434588090cf3df3c3c2af
Sun-Jie Zhang <yahetw@gmail.com>
2023-05-29 16:45:16 +0800

    文字雲

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        List<List<Integer>> listOfLists = new ArrayList<List<Integer>>();
+        HashSet<String> sites = new HashSet<String>();
+
+//        List<List<Integer>> ans = new List<List<Integer>>();
+        int[] y = {53, 26, 37, 94};
+        for (int i =0;i< y.length;i++){
+            System.out.println(y[i]);
+
+        }
     }
 }
